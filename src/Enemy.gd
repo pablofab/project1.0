b80de285f2154fae0344b550d8eef1717cb4b6f4
pablo.fab <pabloarroyomartinez0@gmail.com>
2021-10-28 00:16:53 -0300

    chasing improve

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -1,34 +1,27 @@
 extends KinematicBody
 
-
-
+export var speed = 5
 
 var path = []
-var path_node = 0
-
-var speed = 8
+var cur_path_idx = 0
+var velocity = Vector3.ZERO
+var threshold = .1
 
 onready var nav = get_parent()
-onready var player = $"../../Player"
-
-
-
-func _ready():
-	pass
-
 
 func _physics_process(delta):
-	
-	if path_node < path.size():
-		var direction = (path[path_node] - global_transform.origin)
-		if direction.length() < 1:
-			path_node += 1
-		else:
-			move_and_slide(direction.normalized() * speed, Vector3.UP)	
+	if path.size() > 0:
+		move_to_target()
+		
+func move_to_target():
+	if global_transform.origin.distance_to(path[cur_path_idx]) < threshold:
+		path.remove(0)
+	else:
+		var direction = path[cur_path_idx] - global_transform.origin
+		velocity = direction.normalized() * speed
+		move_and_slide(velocity, Vector3.UP)
+		
+func get_target_path(target_pos):
+	path = nav.get_simple_path(global_transform.origin, target_pos)
 
-func move_to(target_pos):
-	path = nav.get_simple_path(global_transform.origin, target_pos)			
-	path_node = 0
 
-func _on_Timer_timeout():
-	move_to(player.global_transform.origin)
